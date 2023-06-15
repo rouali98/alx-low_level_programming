a060ba06c13ab3561d4b80c7c05b4b2d56973a07
rouali98 <ridaouali98@gmail.com>
2023-06-15 06:33:34 -0700

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * File: 5-printf.c
+ * Description: A C program that prints the string "with proper grammar, but the outcome is a piece of art,"
+ * using the printf function. The program should return 0 and compile without warnings when using the -Wall gcc option.
+*/
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
