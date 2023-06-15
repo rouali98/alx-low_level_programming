7895746f133c12f6285d5bff395d5a7973fcaeb1
rouali98 <ridaouali98@gmail.com>
2023-06-15 06:13:55 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,7 @@
 
 /*
  * File: 4-puts.c
- * Description: A C program that prints the string "Programming is like 
- * building a multilingual puzzle" using the puts function.
+ * Description: A C program that prints a string using the puts function.
  * Return:The program should end with the value 0.
 */
 
