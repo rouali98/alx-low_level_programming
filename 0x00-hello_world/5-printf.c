631f6e18ad442d0a856ba864c58324a35f80b68d
rouali98 <ridaouali98@gmail.com>
2023-06-15 06:46:23 -0700

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,6 @@
  * Description: A C program that prints a string
  * Return: The program should return 0.
 */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
