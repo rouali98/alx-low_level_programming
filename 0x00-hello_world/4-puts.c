6eda3de080096a552ba99d2846931fba235fcb16
rouali98 <ridaouali98@gmail.com>
2023-06-15 06:20:41 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Entry point of the program
+ *
  * Description: A C program that prints a string using the puts function.
  * Return:The program should end with the value 0.
 */
