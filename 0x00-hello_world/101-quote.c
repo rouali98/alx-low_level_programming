671dc8c9d2f4de47074a088b63c0079f7b133d5a
rouali98 <ridaouali98@gmail.com>
2023-06-15 10:54:06 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	printf("%s\n", str);
+	return (1);
+}
