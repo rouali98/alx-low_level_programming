4b58d05a437baafefb7b031a9ba5f922922aec8c
rouali98 <ridaouali98@gmail.com>
2023-06-16 14:10:14 -0700

    print_comb

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,19 +15,22 @@ int main(void)
 	x = 0;
 	while (x <= 7)
 	{
-		y = 1;
+		y = 0;
 		while (y <= 8)
 		{
-			z = 2;
+			z = 0;
 			while (z <= 9)
 			{
-				putchar(x + 48);
-				putchar(y + 48);
-				putchar(z + 48);
-				if (x < 7 || y < 8 || z < 9)
+				if (z > y && y > x)
 				{
-					putchar(',');
-					putchar(' ');
+					putchar(x + 48);
+					putchar(y + 48);
+					putchar(z + 48);
+					if (x < 7 || y < 8 || z < 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 				z++;
 			}
