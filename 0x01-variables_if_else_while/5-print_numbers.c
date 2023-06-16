05312161357076a7861c68f2978dce61c1cfdf4a
rouali98 <ridaouali98@gmail.com>
2023-06-16 12:18:40 -0700

    print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print_numbers
+ *
+ * Description: prints all single digit numbers of base 10 starting from 0.
+ * Return: The program should return 0.
+*/
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+		putchar(i++);
+	putchar('\n');
+	return (0);	
+}
