afe83df49b9cfde33fd86549022ffe1a4be32d10
rouali98 <ridaouali98@gmail.com>
2023-06-16 14:37:12 -0700

    print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -23,8 +23,10 @@ int main(void)
 			putchar((j / 10) + 48);
 			putchar((j % 10) + 48);
 			if (i <= 98 && j <= 99)
+			{
 				putchar(',');
-			putchar(' ');
+				putchar(' ');
+			}
 			j++;
 		}
 		i++;
