4e201721b0ef5241fb9a2b46766bc2f4a1d14221
rouali98 <ridaouali98@gmail.com>
2023-06-22 19:41:23 -0700

    Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,16 @@
 #include "main.h"
-#include "../0x01-variables_if_else_while/0-positive_or_negative.c"
 
 /**
- * main - Entry point
- *
- * Return: Always 0
- */
+* main - a function that check if the num is positive or negative
+* Return: 0
+*/
+
 int main(void)
 {
-    int i;
+	int i;
 
-    i = 0;
-    positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
-    return (0);
+	return (0);
 }
