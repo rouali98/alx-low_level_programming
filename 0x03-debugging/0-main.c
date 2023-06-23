9d8ed8ea62a49dbda11c3d32f91274e93d1af107
rouali98 <ridaouali98@gmail.com>
2023-06-22 19:45:28 -0700

    Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
+* main - a function that check if the num is positive or negative
 * Return: 0
 */
 
