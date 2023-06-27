c60e1ca24db99f730e97d492b5d4a89c8f1095cb
rouali98 <ridaouali98@gmail.com>
2023-06-26 17:01:31 -0700

    reset_to_98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * rest_to_98 - reset a var to 98.
- * @n: the var to rest
- * Return: nothing void
-*/
+ *reset_to_98 - reset a variable to 98
+ *@n: the variable to reset
+ *Return: nothing void
+ */
 
-void rest_to_98(int *n)
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
