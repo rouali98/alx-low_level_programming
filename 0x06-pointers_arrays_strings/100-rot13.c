755c6c6f2ba99f3e6227278625303fe17bcd5cfa
rouali98 <ridaouali98@gmail.com>
2023-07-01 07:36:54 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * *rot13 - a function encode string in rot13
+ * @s: pointer variabel
+ * Return: Always *s when Success
+*/
+
+char *rot13(char *s)
+{
+	int i, j;
+
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (a[j] != '\0')
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (s);
+}
