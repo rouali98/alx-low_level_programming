fe21fd0a464fe8eb9f286532a7b6299b5b8f0892
rouali <rouali@student.1337.ma>
2023-07-13 20:06:10 +0100

    more_malloc_free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -5,6 +5,32 @@
 /* Created BY ROUALI */
 
 /**
+ * _memset - Fills a block of memory with a particular value
+ * @ptr: Pointer to the memory block
+ * @value: Value to be set (treated as unsigned char)
+ * @total_size: Total size of the memory block in bytes
+ * 
+ * Return: Pointer to the memory block (ptr)
+*/
+
+void	*_memset(void *ptr, int value, size_t total_size)
+{
+	unsigned char	*p;
+	size_t			i;
+
+	i = 0;
+	p = (unsigned char *)ptr;
+	while (i < total_size)
+	{
+		p[i] = (unsigned char)value;
+		i++;
+	}
+	return (ptr);
+}
+
+/* Created BY ROUALI */
+
+/**
  * _calloc - The Function
  * @nmemb:  The variable
  * @size:  The variable
@@ -22,6 +48,6 @@ void	*_calloc(unsigned int nmemb, unsigned int size)
 	ptr = malloc(total_size);
 	if (!ptr)
 		return (NULL);
-	memset(ptr, 0, total_size);
+	_memset(ptr, 0, total_size);
 	return (ptr);
 }
