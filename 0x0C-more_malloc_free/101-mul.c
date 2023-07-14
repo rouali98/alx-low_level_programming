b51510d32f18e06e503d2977d1a461894e176f96
rouali <rouali@student.1337.ma>
2023-07-14 19:37:32 +0100

    more_malloc_free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -60,44 +60,53 @@ void errors(void)
 
 int main(int argc, char *argv[])
 {
-	char *s1, *s2;
-	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+    char *s1;
+    char *s2;
 
-	s1 = argv[1], s2 = argv[2];
-	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
-		errors();
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-	len = len1 + len2 + 1;
-	result = malloc(sizeof(int) * len);
-	if (!result)
-		return (1);
-	for (i = 0; i <= len1 + len2; i++)
-		result[i] = 0;
-	for (len1 = len1 - 1; len1 >= 0; len1--)
-	{
-		digit1 = s1[len1] - '0';
-		carry = 0;
-		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
-		{
-			digit2 = s2[len2] - '0';
-			carry += result[len1 + len2 + 1] + (digit1 * digit2);
-			result[len1 + len2 + 1] = carry % 10;
-			carry /= 10;
-		}
-		if (carry > 0)
-			result[len1 + len2 + 1] += carry;
-	}
-	for (i = 0; i < len - 1; i++)
-	{
-		if (result[i])
-			a = 1;
-		if (a)
-			putchar(result[i] + '0');
-	}
-	if (!a)
-		putchar('0');
-	putchar('\n');
-	free(result);
-	return (0);
+    mul.a = 0;
+    s1 = argv[1];
+    s2 = argv[2];
+    if (argc != 3 || !_is_digit(s1) || !_is_digit(s2))
+        errors();
+    mul.len1 = _strlen(s1);
+    mul.len2 = _strlen(s2);
+    mul.len = mul.len1 + mul.len2 + 1;
+    mul.result = malloc(sizeof(int) * mul.len);
+    if (!mul.result)
+        return 1;
+    mul.i = 0;
+    while (mul.i <= mul.len1 + mul.len2)
+        mul.result[mul.i++] = 0;
+    mul.len1 = mul.len1 - 1;
+    while (mul.len1 >= 0)
+    {
+        mul.digit1 = s1[mul.len1] - '0';
+        mul.carry = 0;
+        mul.len2 = _strlen(s2) - 1;
+        while (mul.len2 >= 0)
+        {
+            mul.digit2 = s2[mul.len2] - '0';
+            mul.carry += mul.result[mul.len1 + mul.len2 + 1] + (mul.digit1 * mul.digit2);
+            mul.result[mul.len1 + mul.len2 + 1] = mul.carry % 10;
+            mul.carry /= 10;
+            mul.len2--;
+        }
+        if (mul.carry > 0)
+            mul.result[mul.len1 + mul.len2 + 1] += mul.carry;
+        mul.len1--;
+    }
+    mul.i = 0;
+    while (mul.i < mul.len - 1)
+    {
+        if (mul.result[mul.i])
+            mul.a = 1;
+        if (mul.a)
+            _putchar(mul.result[mul.i] + '0');
+        mul.i++;
+    }
+    if (!mul.a)
+        _putchar('0');
+    _putchar('\n');
+    free(mul.result);
+    return (0);
 }
