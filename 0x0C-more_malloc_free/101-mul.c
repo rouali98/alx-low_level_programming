823eabe7af5aec57d19682131c497c1b039877d4
rouali <rouali@student.1337.ma>
2023-07-14 16:15:43 +0100

    more_malloc_free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,84 +1,123 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
 
-/* Created BY ROUALI */
+struct s_multip
+{
+	int	len1;
+	int	len2;
+	int	len;
+	int	i;
+	int	carry;
+	int	digit1;
+	int	digit2;
+	int	*result;
+	int	a;
+
+} mul;
 
 /**
-  * _isdigit - tells if the string consists of digits
-  * @argv: pointer to current item in argument
-  * Return: return 0 if all digits, 1 if not all digits.
-*/
-
-int	_isdigit(char *argv)
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
+int	_is_digit(char *s)
 {
 	int	i;
 
 	i = 0;
-	while (argv[i])
+	while (s[i])
 	{
-		if (argv[i] >= '0' && argv[i] <= '9')
-			i++;
-		else
-			return (1);
-	}
-	return (0);
-}
-
-/* Created BY ROUALI */
-
-/**
-  * _atoi - converts a string of ascii digits to the values they represent
-  * @s: pointer to the source string
-  * Return: value of digits
-*/
-
-int	_atoi(char *str)
-{
-	int	i;
-	int	result;
-
-	result = 0;
-	i = result;
-	while (str[i])
-	{
-		if (str[i] >= '0' && str[i] <= '9')
-		{
-			result *= 10;
-			result += (str[i] - '0');
-		}
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
 		i++;
 	}
-	return (result);
+	return (1);
 }
 
-/* Created BY ROUALI */
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int	_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
 
 /**
-  * main - main function call
-  * @argc: argument count
-  * @argv: 2D array of arguments
-  * Return: return 0 on success, 98 on failure
-*/
+ * errors - handles errors for main
+ */
+void	errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
 
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
+ */
 int	main(int argc, char *argv[])
 {
-	int	i;
+	char	*s1;
+	char	*s2;
 
-	malloc();
-	if (argc != 3)
+	struct mul;
+	mul.a = 0;
+	s1 = argv[1];
+	s2 = argv[2];
+	if (argc != 3 || !_is_digit(s1) || !_is_digit(s2))
+		errors();
+	mul.len1 = _strlen(s1);
+	mul.len2 = _strlen(s2);
+	mul.len = mul.len1 + mul.len2 + 1;
+	mul.result = malloc(sizeof(int) * mul.len);
+	if (!mul.result)
+		return (1);
+	mul.i = 0;
+	while (mul.i <= mul.len1 + mul.len2)
+		mul.result[mul.i++] = 0;
+	mul.len1 = mul.len1 - 1;
+	while (mul.len1 >= 0)
 	{
-		printf("Error\n");
-		exit(98);
-	}
-	i = 1;
-	while (i < argc)
-	{
-		if (_isdigit(argv[i]))
+		mul.digit1 = s1[mul.len1] - '0';
+		mul.carry = 0;
+		mul.len2 = _strlen(s2) - 1;
+		while (mul.len2 >= 0)
 		{
-			printf("Error\n");
-			exit(98);
+			mul.digit2 = s2[mul.len2] - '0';
+			mul.carry += mul.result[mul.len1 + mul.len2 + 1] + (mul.digit1 * mul.digit2);
+			mul.result[mul.len1 + mul.len2 + 1] = mul.carry % 10;
+			mul.carry /= 10;
+			mul.len2--;
 		}
-		i++;
+		if (mul.carry > 0)
+			mul.result[mul.len1 + mul.len2 + 1] += mul.carry;
+		mul.len1--;
 	}
+	mul.i = 0; 
+	while (mul.i < mul.len - 1)
+	{
+		if (mul.result[mul.i])
+			mul.a = 1;
+		if (mul.a)
+			putchar(mul.result[mul.i] + '0');
+		mul.i++;
+	}
+	if (!mul.a)
+		_putchar('0');
+	_putchar('\n');
+	free(mul.result);
 	return (0);
 }
---
+++ 0x0C-more_malloc_free/_putchar.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <unistd.h>
+
+/* Created BY ROUALI */
+
+void	_putchar(char c)
+{
+	write(1, &c, 1);
+}
