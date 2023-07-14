517519e494f084eaf522dccb56e18c7a5446ff52
rouali <rouali@student.1337.ma>
2023-07-14 19:49:31 +0100

    more_malloc_free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -42,7 +42,7 @@ int _strlen(char *s)
 /**
  * errors - handles errors for main
  */
-void _errors(void)
+void errors(void)
 {
 	write(1, "Error\n", 6);
 	exit(98);
@@ -62,7 +62,7 @@ int main(int argc, char *argv[])
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
-		_errors();
+		errors();
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
