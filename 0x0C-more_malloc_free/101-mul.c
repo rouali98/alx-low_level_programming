766bd1b20f320d64e3fae6d854f41e0f0a43ea16
rouali <rouali@student.1337.ma>
2023-07-14 13:24:41 +0100

    more_malloc_free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,57 +5,79 @@
 /* Created BY ROUALI */
 
 /**
- * main - multiply 2 numbers as arguments
- * @argc: number of arguments
- * @argv: arguments
- * Return: 0 success
+  * _isdigit - tells if the string consists of digits
+  * @argv: pointer to current item in argument
+  * Return: return 0 if all digits, 1 if not all digits.
 */
 
-void	multiply(const char *num1, const char *num2)
+int	_isdigit(char *argv)
 {
-	int		mul;
-	int		pos1;
-	int		pos2;
-	int		sum;
-	int		len1;
-	int		len2;
-	int		i;
-	int		j;
-	int		*result;
-	char	digit;
+	int	i;
 
-	len1 = 0;
-	len2 = 0;
-	while (!num1[len1])
-		len1++;
-	while (!num2[len2])
-		len2++;
-	result = _calloc(len1 + len2, sizeof(int));
-	i = len1 - 1;
-	while (i >= 0)
-	{
-		j = len2 - 1;
-		while (j >= 0)
-		{
-			mul = (num1[i] - '0') * (num2[j] - '0');
-			pos1 = i + j;
-			pos2 = i + j + 1;
-			sum = mul + result[pos2];
-			result[pos1] += sum / 10;
-			result[pos2] = sum % 10;
-			j--;
-		}
-		i--;
-	}
 	i = 0;
-	while (result[i] == 0 && i < len1 + len2 - 1)
-		i++;
-	while (i < len1 + len2)
+	while (argv[i])
 	{
-		digit = result[i] + '0';
-		write(1, &digit, 1);
+		if (argv[i] >= '0' && argv[i] <= '9')
+			i++;
+		else
+			return (1);
+	}
+	return (0);
+}
+/* Created BY ROUALI */
+
+/**
+  * _atoi - converts a string of ascii digits to the values they represent
+  * @s: pointer to the source string
+  * Return: value of digits
+  */
+int	_atoi(char *s)
+{
+	int	i;
+	int	result;
+
+	result = 0;
+	i = result;
+	while (s[i])
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result *= 10;
+			result += (s[i] - '0');
+		}
 		i++;
 	}
-	write(1, "\n", 1);
-	free(result);
+	return (result);
+}
+
+/* Created BY ROUALI */
+
+/**
+  * main - main function call
+  * @argc: argument count
+  * @argv: 2D array of arguments
+  * Return: return 0 on success, 98 on failure
+*/
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+
+	malloc();
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	i = 1;
+	while (i < argc)
+	{
+		if (_isdigit(argv[i]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+		i++;
+	}
+	return (0);
 }
