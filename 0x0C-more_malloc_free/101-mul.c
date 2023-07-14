62eb1d1d933e5fdff909e8506e16112b4a7df136
rouali <rouali@student.1337.ma>
2023-07-14 19:45:36 +0100

    more_malloc_free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,7 +9,7 @@
  *
  * Return: 0 if a non-digit is found, 1 otherwise
  */
-int is_digit(char *s)
+int _is_digit(char *s)
 {
 	int i = 0;
 
@@ -42,7 +42,7 @@ int _strlen(char *s)
 /**
  * errors - handles errors for main
  */
-void errors(void)
+void _errors(void)
 {
 	write(1, "Error\n", 6);
 	exit(98);
@@ -61,8 +61,8 @@ int main(int argc, char *argv[])
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
-	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
-		errors();
+	if (argc != 3 || !_is_digit(s1) || !_is_digit(s2))
+		_errors();
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
