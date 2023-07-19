5e9fd8edc835802e32cbe6973f05435615d889c7
rouali <rouali@student.1337.ma>
2023-07-19 21:44:30 +0100

    0x0F-function_pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -3,14 +3,6 @@
 
 /* Created BY ROUALI */
 
-/**
- * get_op_func - Selects the correct function based on the operator.
- * @s: The operator passed as an argument to the program.
- *
- * Return: A pointer to the function that corresponds to the operator given as a parameter.
- *         If no matching operator is found, returns NULL.
-*/
-
 int (*get_op_func(char *s))(int, int)
 {
 	int	i;
