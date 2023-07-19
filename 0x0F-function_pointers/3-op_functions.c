6333faf1e471d705f30696ec05a25fee16dbb495
rouali <rouali@student.1337.ma>
2023-07-19 21:42:05 +0100

    0x0F-function_pointers

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -2,70 +2,25 @@
 
 /* Created BY ROUALI */
 
-/**
- * op_add - Performs addition of two integers.
- * @a: The first integer.
- * @b: The second integer.
- *
- * Return: The result of the addition.
-*/
-
 int	op_add(int a, int b)
 {
 	return (a + b);
 }
-/* Created BY ROUALI */
-
-/**
- * op_sub - Performs subtraction of two integers.
- * @a: The first integer.
- * @b: The second integer.
- *
- * Return: The result of the subtraction.
-*/
 
 int	op_sub(int a, int b)
 {
 	return (a - b);
 }
-/* Created BY ROUALI */
-
-/**
- * op_mul - Performs multiplication of two integers.
- * @a: The first integer.
- * @b: The second integer.
- *
- * Return: The result of the multiplication.
-*/
-
 
 int	op_mul(int a, int b)
 {
 	return (a * b);
 }
-/* Created BY ROUALI */
-
-/**
- * op_div - Performs division of two integers.
- * @a: The numerator.
- * @b: The denominator.
- *
- * Return: The result of the division.
-*/
 
 int	op_div(int a, int b)
 {
 	return (a / b);
 }
-/* Created BY ROUALI */
-
-/**
- * op_mod - Performs modulo operation on two integers.
- * @a: The dividend.
- * @b: The divisor.
- *
- * Return: The result of the modulo operation.
-*/
 
 int	op_mod(int a, int b)
 {
