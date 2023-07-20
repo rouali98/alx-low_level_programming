4e9f9f70ae6ffada535bebe66c33ccb431a58220
rouali <rouali@student.1337.ma>
2023-07-20 13:10:03 +0100

    Function_pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -14,19 +14,20 @@
 
 int (*get_op_func(char *s))(int, int)
 {
-	int	i;
-
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
-		{NULL, NULL}};
+		{NULL, NULL}
+	};
+	int i;
+
 	i = 0;
-	while (ops[i].op != NULL)
+	while (ops[i].op)
 	{
-		if (*(ops[i].op) == *s)
+		if (s[0] == ops[i].op[0])
 			return (ops[i].f);
 		i++;
 	}
