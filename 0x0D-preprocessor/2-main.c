eab8c5eaea818aed345c428032971de996247723
rouali <rouali@student.1337.ma>
2023-07-21 09:59:09 +0100

    Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* Created BY ROUALI */
+
+/**
+ * main - Prints the name of the file it was compiled from.
+ *
+ * Return: Always 0.
+*/
+
+int main(void)
+{
+    printf("%s\n", __FILE__);
+    return (0);
+}
